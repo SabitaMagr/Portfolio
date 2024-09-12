812903958ab17b7f837519abb0079314f47f9512
SabitaMagr <sabitathapa9864@gmail.com>
2024-09-12 08:15:51 +0545

    js/cs/angularjs

---
+++ Portfolio/wwwroot/assets/js/module/portfolioModule.js
@@ -0,0 +1,28 @@
+﻿var PortfolioModule = angular.module('PortfolioModule', ['ngRoute', 'ngMessages', 'datatables', 'kendo.directives', 'cfp.hotkeys', 'ngHandsontable', 'angularjs-dropdown-multiselect']).run(['$rootscope', function ($rootscope) {
+
+}]);
+PortfolioModule.config(function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/',
+        {
+            templateUrl: function () {
+                return 'Portfolio/Home/Home';
+            }
+        });
+    $routeProvider.when('/Login',
+        {
+            templateUrl: function () {
+                return '/Portfolio/Home/Login';
+            }
+        });
+    $routeProvider.when('/About',
+        {
+            templateUrl: function () {
+            return 'Portfolio/Home/About';
+            }
+        });
+    $routeProvider.when('Contact', {
+        templateUrl: function () {
+            return 'Portfolio/Home/Contact';
+        }
+    });
+})
